366499d6c908066a4e77ed603253fe60df8023d5
aguiar-tiago <tiago.aguiar@proposify.com>
2021-12-02 15:09:01 -0400

    update: misc

--- components/Login.js
+++ components/Login.js
@@ -13,11 +13,11 @@ const Login = () => {
                     </div>
                     <form action="" className="w-full">
                         <div className="my-5 text-sm">
-                            <label for="username" className="block text-black">Username</label>
+                            <label htmlFor="username" className="block text-black">Username</label>
                             <input type="text" autoFocus id="username" className="bg-white rounded-sm px-4 py-3 mt-3 focus:outline-none  w-full" placeholder="Username" />
                         </div>
                         <div className="my-5 text-sm">
-                            <label for="password" className="block text-black">Password</label>
+                            <label htmlFor="password" className="block text-black">Password</label>
                             <input type="password" id="password" className="bg-white rounded-sm px-4 py-3 mt-3 focus:outline-none  w-full" placeholder="Password" />
                             <div className="flex justify-end mt-2 text-xs text-gray-600">
                             <a href="#">Forgot Password?</a>
@@ -27,13 +27,13 @@ const Login = () => {
                         <button className="block text-center text-white bg-gray-800 p-3 duration-300 rounded-sm hover:bg-black w-full">Login</button>
                     </form>
 
-                    <div class="flex md:justify-between justify-center items-center mt-10 w-full">
-                        <div style={{"height": "1px;"}} class="bg-gray-300 md:block hidden w-4/12"></div>
-                        <p class="md:mx-2 text-sm font-light text-gray-400"> Login With Social </p> 
-                        <div style={{"height": "1px;"}} class="bg-gray-300 md:block hidden w-4/12"></div>
+                    <div className="flex md:justify-between justify-center items-center mt-10 w-full">
+                        <div style={{"height": "1px"}} className="bg-gray-300 md:block hidden w-4/12"></div>
+                        <p className="md:mx-2 text-sm font-light text-gray-400"> Login With Social </p> 
+                        <div style={{"height": "1px"}} className="bg-gray-300 md:block hidden w-4/12"></div>
                     </div>
 
-                    <div class="grid md:grid-cols-2 gap-2 mt-7">
+                    <div className="grid md:grid-cols-2 gap-2 mt-7">
                         <div>
                             <button className="text-center w-full text-white bg-blue-900 p-3 duration-300 rounded-sm hover:bg-blue-700">Facebook</button>
                         </div>
--- components/TextEditor.js
+++ components/TextEditor.js
@@ -58,7 +58,7 @@ function TextEditor() {
         editorState={editorState}
         onEditorStateChange={onEditorStateChange}
         toolbarClassName="flex sticky top-0 z-50 !justify-center mx-auto"
-        editorClassName="mt-6 p-10 bg-white shadow-lg max-w-5xl mx-auto mb-12 border"
+        editorClassName="mt-6 p-10 bg-white shadow-lg min-h-screen max-w-5xl mx-auto mb-12 border"
       />
     </div>
   );
