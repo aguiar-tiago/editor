616765e7be76d8f9a372a10c2871276ef12da1a2
aguiar-tiago <tiago.aguiar@proposify.com>
2021-12-13 10:29:11 -0400

    add: styles on signature component

--- components/elements/signature/Signature.js
+++ components/elements/signature/Signature.js
@@ -4,24 +4,19 @@ export const Signature = ({blockProps}) => {
         <button 
             type="button" 
             className="
-                flex max-w-sm 
-                bg-gradient-to-r
-                from-indigo-500 
-                via-pink-500 
-                to-yellow-500 
-                hover:from-indigo-600 
-                hover:via-pink-600 
-                hover:to-red-600 
+                flex
+                bg-black
                 focus:outline-none
-                text-white 
-                text-2xl
-                uppercase 
+                text-white
+                text-sm
+                uppercase
                 font-bold
                 shadow-md
-                p-5">
+                rounded-sm
+                p-2">
                 <div className="flex sm:flex-cols-12 gap-6">
                     <div className="col-span-1">🖊️</div>
-                    <div className="col-span-2 pt-1.5">signature</div>
+                    <div className="col-span-2">signature</div>
                 </div> 
         </button>
     );
